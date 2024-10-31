bf41586a6903113d7e401474dad921c51512d9ff
AdrianC2095 <adriancuenca095@gmail.com>
2024-10-31 00:16:20 -0500

    Ejercicios Terminados

--- Proyecto_Taller4/src/Ejercicio5.java
+++ Proyecto_Taller4/src/Ejercicio5.java
@@ -1,12 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author UTPL
- */
+import java.util.Scanner;
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        double costCPU, costTeclado, costPantalla, costMouse, costPC;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("INGRESE PRECIO DEL CPU: ");
+        costCPU = teclado.nextDouble();
+        System.out.print("INGRESE PRECIO DEL TECLADO: ");
+        costTeclado = teclado.nextDouble();
+        System.out.print("INGRESE PRECIO DE LA PANTALLA: ");
+        costPantalla = teclado.nextDouble();
+        System.out.print("INGRESE PRECIO DEL MOUSE: ");
+        costMouse = teclado.nextDouble();
+        costPC = costCPU + costTeclado + costPantalla + costMouse;
+        System.out.println("EL COSTO DE LA COMPUTADORA ES = " + costPC);
+    }
     
 }
--- Proyecto_Taller4/src/Ejercicio6.java
+++ Proyecto_Taller4/src/Ejercicio6.java
@@ -1,12 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author UTPL
- */
+import java.util.Scanner;
 public class Ejercicio6 {
-    
+    public static void main(String[] args) {
+        double montoprestamo, intermes, pagomes, pagomes_sininter, valorinteresmes;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("INGRESE MONTO DEL PRESTAMO: ");
+        montoprestamo = teclado.nextDouble();
+        System.out.print("INGRESE EL PORCENTAJE DE INTERES: ");
+        intermes = teclado.nextDouble();
+        pagomes_sininter = montoprestamo / 12;
+        valorinteresmes = pagomes_sininter * (intermes / 100);        
+        pagomes = pagomes_sininter + valorinteresmes;
+        System.out.println("EL VALOR A PAGAR MENSUALMENTE ES = " + pagomes);
+    }
 }
